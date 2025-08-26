d3486fda1ecf78e7794fc6d5362b2a5ed8c49d71
Dannnzzzz <danngomig@gmail.com>
2025-08-25 21:17:26 -0300

    First commit

---
+++ Atividades 2/Recursive/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter the length of the Fibonacci Series: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        string input = Console.ReadLine();
+        
+        int lenght = 0;
+        if (!Int32.TryParse(input, out lenght))
+        {
+            Console.WriteLine("Número invalido.");
+            return;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("{0} ", FibonacciSeries(i));
+        }
+
+        Console.ReadKey();
+
+    static int FibonacciSeries(int n)
+    {
+        if (n == 0) return 0; // To return the first Fibonacci number
+        if (n == 1) return 1; // To return the second Fibonacci number
+        return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+    }
---
+++ Atividades/JaggedArrays/Program.cs
@@ -0,0 +1,37 @@
+﻿// JaggedArrays
+//São Arrays de Arrays
+// Ou também 'Arrays Irregulares'
+int [][] numbers = 
+    new int [4][]; 
+numbers[0] = 
+    new int[] {1, 2, 3};
+numbers[1] =
+    new int[] {4};
+numbers[2] =
+    new int[] {5, 6, 7, 8, 9};
+numbers[3] = 
+    new int[] {6, 5, 4}
+
+    // outra forma de inicializar
+
+int [][] numbers2 =
+{
+    new int[] {1}
+    new int[] {2}
+    new int[] {3}
+    new int[] {4}
+};
+
+//Para percorrer o JaggedArray
+//Utilizamos laços de repetição
+// encadeados / nested loop
+for(int i = 0; i < numbers.Lenght; i++)
+{
+    Console.Write("[]")
+    for (int j = 0; j < numbers[i]. Lenght; j++)
+    {
+        Console.Write($" {numbers[i][j]} ");
+    }
+    Console.Write("]");
+        
+}
