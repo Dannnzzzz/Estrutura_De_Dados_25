b6906747266639c23b7383c283132c38c5529c3f
Dannnzzzz <danngomig@gmail.com>
2025-10-07 21:44:14 -0300

    Add files via upload

---
+++ Atividades/Bubble/Program.cs
@@ -0,0 +1,25 @@
+﻿using bubble;
+
+Console.WriteLine("Ordenção com Bobble");
+int[] arrNumber = new int[] { 99, 50, -24, 0, 1 };
+string[] arrString = new string[] { "Golfinho", "Elefante", "Girafa", "Camaleao" };
+
+foreach (var number in arrNumber)
+{
+    Console.Write($"[{number}]");
+}
+
+var arOrdered = BubbleSortOrder.Sort<int>(arrNumber);
+
+var arrStringordered = BubbleSortOrder.Sort<string>(arrString);
+Console.WriteLine("vetor ordenado");
+
+foreach (var number in arOrdered)
+{
+    Console.Write($"[{number}]");
+}
+
+foreach (var str in arrStringordered)
+{
+    Console.Write($"[{str}]");
+}
